68fc2240dde74a6a25ae7923753f6fd5bc5e69ae
wubeshet <wubeshet@DESKTOP-KE41FSF.localdomain>
2023-09-07 19:21:46 +0300

    update

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Calculates the shape of a matrix
 """
+
+
 def matrix_shape(matrix):
     """ Calculates the shape of a matrix """
     shape = []
@@ -8,13 +10,3 @@ def matrix_shape(matrix):
         shape.append(len(matrix))
         matrix = matrix[0]
     return shape
-
-# mat1 = [1,1]
-# print(matrix_shape(mat1))
-# mat2 = [[[1, 2, 3, 4, 5],
-#         [6, 7, 8, 9, 10],
-#          [11, 12, 13, 14, 15]],
-#         [[16, 17, 18, 19, 20],
-#          [21, 22, 23, 24, 25],
-#          [26, 27, 28, 29, 30]]]
-# print(matrix_shape(mat2))
