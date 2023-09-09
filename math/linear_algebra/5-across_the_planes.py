53beb1887b0938568fc4f274910cd7607a7d7acc
Wubeshet Yimam <w.yimam@alustudent.com>
2023-09-09 11:20:44 +0300

    Add task6

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -25,9 +25,9 @@ def add_matrices2D(mat1, mat2):
     return sum_array
 
 
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6], [7, 8]]
-print(add_matrices2D(mat1, mat2))
-print(mat1)
-print(mat2)
-print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
+# mat1 = [[1, 2], [3, 4]]
+# mat2 = [[5, 6], [7, 8]]
+# print(add_matrices2D(mat1, mat2))
+# print(mat1)
+# print(mat2)
+# print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def cat_arrays(arr1, arr2):
+    return arr1 + arr2
+
+
+# arr1 = [1, 2, 3, 4, 5]
+# arr2 = [6, 7, 8]
+# print(cat_arrays(arr1, arr2))
+# print(arr1)
+# print(arr2)
