b7fdc059479453ccafb05d7e1053cf291af6bb01
Wubeshet Yimam <w.yimam@alustudent.com>
2023-09-10 18:17:17 +0300

    add task 11

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Calculate shape of a matrix
+"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """Calculate the shape of a numpy matrix"""
+    return matrix.shape
---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Calculate transpose of a matrix
+"""
+import numpy as np
+
+
+def np_transpose(matrix):
+    """Returns a transpose of a matrix
+
+    Args:
+        matrix (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return matrix.transpose()
