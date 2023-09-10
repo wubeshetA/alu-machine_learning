68b4d221a98a44c69aaa6a1d56b8b2b0814e70a5
Wubeshet Yimam <w.yimam@alustudent.com>
2023-09-10 18:33:22 +0300

    Add task 12

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Function that performs elementwise operations
+"""
+import numpy as np
+
+
+def np_elementwise(mat1, mat2):
+    """Returns a tuple consisting of elementwise operations
+
+    Args:
+        mat1 (_type_): _description_
+        mat2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return (add, sub, mul, div)
