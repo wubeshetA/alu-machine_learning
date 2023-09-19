e1c45584b147fac1f4657d7b0109cdac98462d4a
Wubeshet Yimam <w.yimam@alustudent.com>
2023-09-20 02:05:17 +0300

    update answer

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -11,13 +11,11 @@ def poly_integral(poly, C=0):
     if not isinstance(poly, list) or len(poly) == 0 or not isinstance(C, int):
         return None
 
-    # integral.append(c)
     for i in range(len(poly)-1, 0, -1):
 
         integral.append(poly[i]/(i+1))
     integral.append(poly[0])
-    if len(integral) > 1:
-        integral.append(C)
+    integral.append(C)
     for i in range(len(integral)):
         if integral[i] % 1 == 0:
             integral[i] = int(integral[i])
@@ -25,5 +23,5 @@ def poly_integral(poly, C=0):
     return integral[::-1]
 
 
-poly = [5]
-print(poly_integral(poly, C=4))
+# poly = [5]
+# print(poly_integral(poly, C=0))
