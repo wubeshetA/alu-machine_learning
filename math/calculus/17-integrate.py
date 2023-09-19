ef73588a9128480f6ef25803d6c552270915f98e
Wubeshet Yimam <w.yimam@alustudent.com>
2023-09-20 02:11:41 +0300

    update answer

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -8,20 +8,19 @@ def poly_integral(poly, C=0):
     """ Calculates the integral of a polynomial
     """
     integral = []
+    # check for the validity of the inputs
     if not isinstance(poly, list) or len(poly) == 0 or not isinstance(C, int):
         return None
-
+    # calculate the cofficients
     for i in range(len(poly)-1, 0, -1):
-
         integral.append(poly[i]/(i+1))
+
     integral.append(poly[0])
-    integral.append(C)
+    if C:
+        integral.append(C)
+    # fit the output like on the intranet
     for i in range(len(integral)):
         if integral[i] % 1 == 0:
             integral[i] = int(integral[i])
 
     return integral[::-1]
-
-
-# poly = [5]
-# print(poly_integral(poly, C=0))
