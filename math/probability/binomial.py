8ffbb126abbabe3af415fe69419dfc796b370c28
Wubeshet Yimam <w.yimam@alustudent.com>
2023-09-30 14:11:59 +0300

    Fix logic

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -8,32 +8,34 @@ class Binomial:
     """Binomial class"""
 
     def __init__(self, data=None, n=1, p=0.5):
-        """ Constructor
+        """_summary_
 
         Args:
-            data (list, optional): list of the data to be used.
-            n (int, optional): is the number of Bernoulli trials.
-            p (float, optional): probablity of success. Defaults to 0.5.
+            data (_type_, optional): _description_. Defaults to None.
+            n (int, optional): _description_. Defaults to 1.
+            p (float, optional): _description_. Defaults to 0.5.
+
+        Raises:
+            ValueError: _description_
+            ValueError: _description_
+            TypeError: _description_
+            ValueError: _description_
         """
-
-        self.n = int(n)
-        self.p = float(p)
-
-        if n < 0:
-            raise ValueError("n must be a positive value")
-        if p < 0 or p > 1:
-            raise ValueError("p must be greater than 0 and less than 1")
-        if data:
-            if not isinstance(data, list):
+        if data is None:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            if p <= 0 or p >= 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            if type(data) != list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            # Hint: Calculate p first and then calculate n.
-            # Then recalculate p. Think about why you would
-            # want to do it this way?
             mean = float(sum(data) / len(data))
-            var = float(
-                (sum(map(lambda n: pow(n - mean, 2), data)) / len(data)))
+            var = float((sum(map(lambda n: pow(n - mean,
+                        2), data)) / len(data)))
             self.p = - (var / mean) + 1
             self.n = round(mean / self.p)
             self.p = mean / self.n
