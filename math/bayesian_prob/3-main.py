adb4999209c09731225ccffd37210c4b2b9131d9
Wubeshet Yimam <w.yimam@alustudent.com>
2023-11-05 11:57:44 +0300

    Add task3

--- math/bayesian_prob/3-main.py
+++ math/bayesian_prob/3-main.py
@@ -2,8 +2,8 @@
 
 if __name__ == '__main__':
     import numpy as np
-    marginal = __import__('2-marginal').marginal
+    posterior = __import__('3-posterior').posterior
 
     P = np.linspace(0, 1, 11)
     Pr = np.ones(11) / 11
-    print(marginal(26, 130, P, Pr))
+    print(posterior(26, 130, P, Pr))
