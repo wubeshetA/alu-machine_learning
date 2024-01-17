17a5708994f661b71fa93751872509de42863a6e
Wubeshet Yimam <w.yimam@alustudent.com>
2024-01-18 00:10:10 +0200

    Add forwar propagation function

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -22,9 +22,10 @@ class Neuron:
         """
         if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
-        if nx < 1:
+        if nx < 0:
             raise ValueError('nx must be positive')
 
-        self.W = np.random.normal(0, 1, (1, nx))
+        self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A = 0
+        
