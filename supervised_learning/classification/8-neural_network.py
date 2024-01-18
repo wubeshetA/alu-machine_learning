57ca53a850abac1e66374760b2d71d4a19aa42ec
Wubeshet Yimam <w.yimam@alustudent.com>
2024-01-18 18:14:12 +0200

    Fix error

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -30,6 +30,6 @@ class NeuralNetwork:
         self.W1 = np.random.randn(nodes, nx)
         self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
-        self.W2 = np.random.randn(size=(1, nodes))
+        self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
         self.A2 = 0
