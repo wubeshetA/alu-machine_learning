f9a98eedbcfebca37b16e00b195b5c38b76d8d77
Wubeshet Yimam <w.yimam@alustudent.com>
2024-01-19 10:40:04 +0200

    Fix error

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -21,7 +21,7 @@ class DeepNeuralNetwork:
         if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError('nx must a be positive')
+            raise ValueError('nx must be a positive integer')
 
         if not isinstance(layers, list):
             raise TypeError('layers must be a list of positive integers')
@@ -37,13 +37,13 @@ class DeepNeuralNetwork:
                 raise TypeError('layers must be a list of positive integers')
 
             if i == 0:
-                # He et al. initialization for weights in first layer
+                # He et al. initialization
                 self.weights['W' + str(i + 1)] = np.random.randn(
                     layers[i], nx) * np.sqrt(2 / nx)
             else:
-                # He et al. initialization for weights for the rest of layers
+                # He et al. initialization
                 self.weights['W' + str(i + 1)] = np.random.randn(
                     layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
 
-            # Zero initialization for biases
+            # Zero initialization
             self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
