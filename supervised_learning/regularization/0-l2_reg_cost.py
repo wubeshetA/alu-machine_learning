b2cf47dc0cec8cd34e35f731d968c751980df18c
Wubeshet Yimam <w.yimam@alustudent.com>
2024-01-31 16:26:19 +0200

    Add gradient descent with regularization

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-
-
-import numpy as np
 """
  Calculates the cost of a neural network with L2 regularization
 """
 
+import numpy as np
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """ Calculates the cost of a neural network with L2 regularization
