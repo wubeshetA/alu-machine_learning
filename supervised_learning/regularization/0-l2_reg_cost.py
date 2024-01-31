a549b9d6f16801984b6be87779100d1b6575f7e5
Wubeshet Yimam <w.yimam@alustudent.com>
2024-01-31 13:01:08 +0200

    Add task 0

---
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+import numpy as np
+"""
+ Calculates the cost of a neural network with L2 regularization
+"""
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """ Calculates the cost of a neural network with L2 regularization
+
+    Args:
+        cost (float): the cost of the network without L2 regularization
+        lambtha (_type_):  is the regularization parameter
+        weights (_type_): s a dictionary of the weights and biases
+        (numpy.ndarrays) of the neural network
+        L (int): the number of layers in the neural network
+        m (int): the number of data points used
+    """
+    penality = 0
+
+    for i in range(1, L + 1):
+        key = 'W' + str(i)
+        penality += np.sum(np.square(weights[key]))
+
+    penality *= (lambtha / (2 * m))
+
+    total_cost = cost + penality
+
+    return total_cost
