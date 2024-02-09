3322fdaf4e0633ac8a5bde0b29b5152637b5664e
Wubeshet Yimam <w.yimam@alustudent.com>
2024-02-09 11:22:44 +0200

    Hot fix

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -27,55 +27,39 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
     Returns:
         str: the path where the model was saved
     """
-
-    tf.reset_default_graph()
     with tf.Session() as sess:
-
-        # load graph
         saver = tf.train.import_meta_graph(load_path + '.meta')
         saver.restore(sess, load_path)
-
-        # Get tensors and ops
         x = tf.get_collection('x')[0]
         y = tf.get_collection('y')[0]
+        train_op = tf.get_collection('train_op')[0]
         accuracy = tf.get_collection('accuracy')[0]
         loss = tf.get_collection('loss')[0]
-        train_op = tf.get_collection('train_op')[0]
-
-        batch = X_train.shape[0] // batch_size
+        size = X_train.shape[0] // batch_size
         if X_train.shape[0] % batch_size != 0:
-            batch += 1  # add 1 if there are remaining data points
-        for epoch in range(epochs+1):
-
-            train_cost, train_accuracy = sess.run(
-                [loss, accuracy], feed_dict={x: X_train, y: Y_train})
-            valid_cost, valid_accuracy = sess.run(
-                [loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
-
-            print("After {} epochs:".format(epoch))
-            print("\tTraining Cost: {}".format(train_cost))
-            print("\tTraining Accuracy: {}".format(train_accuracy))
-            print("\tValidation Cost: {}".format(valid_cost))
-            print("\tValidation Accuracy: {}".format(valid_accuracy))
-
-            if epoch < epochs:
+            size += 1
+        for i in range(epochs + 1):
+            cost_t, acc_t = sess.run([loss, accuracy],
+                                     feed_dict={x: X_train, y: Y_train})
+            cost_v, acc_v = sess.run([loss, accuracy],
+                                     feed_dict={x: X_valid, y: Y_valid})
+            print("After {} epochs:".format(i))
+            print("\tTraining Cost: {}".format(cost_t))
+            print("\tTraining Accuracy: {}".format(acc_t))
+            print("\tValidation Cost: {}".format(cost_v))
+            print("\tValidation Accuracy: {}".format(acc_v))
+            if i < epochs:
                 x_shuffled, y_shuffled = shuffle_data(X_train, Y_train)
-                for i in range(batch):
-                    # # print every 100 steps
-                    # if step % 100 == 0:
-                    #     print(i)
+                for i in range(size):
                     start = i * batch_size
                     end = start + batch_size
-                    x_batch = x_shuffled[start:end]
-                    y_batch = y_shuffled[start:end]
-
-                    sess.run(train_op, feed_dict={x: x_batch, y: y_batch})
-                    if (i+1) % 100 == 0 and i > 0:
-                        step_cost, step_accuracy = sess.run(
-                            [loss, accuracy], feed_dict={x: x_batch, y: y_batch})
-                        print("\tStep {}: ".format(i+1))
-                        print("\t\tCost: {}".format(step_cost))
-                        print("\t\tAccuracy: {}".format(step_accuracy))
-
-        save_path = saver.save(sess, save_path)
-    return save_path
+                    x_mini = x_shuffled[start:end]
+                    y_mini = y_shuffled[start:end]
+                    sess.run(train_op, feed_dict={x: x_mini, y: y_mini})
+                    if (i + 1) % 100 == 0 and i > 0:
+                        cost, acc = sess.run([loss, accuracy],
+                                             feed_dict={x: x_mini, y: y_mini})
+                        print("\tStep {}:".format(i + 1))
+                        print("\t\tCost: {}".format(cost))
+                        print("\t\tAccuracy: {}".format(acc))
+        return saver.save(sess, save_path)
