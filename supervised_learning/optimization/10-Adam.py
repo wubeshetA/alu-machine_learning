226d4838f8a4d9b44eb80320d72df894df65f9e2
Wubeshet Yimam <w.yimam@alustudent.com>
2024-02-09 21:35:15 +0200

    fix code style

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -5,6 +5,7 @@
 
 import tensorflow as tf
 
+
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """ creates the training operation for a neural network in tensorflow
         using the Adam optimization algorithm
