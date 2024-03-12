6e118582248db46e42f8e9fbc604cd4b630351bc
Wubeshet Yimam <w.yimam@alustudent.com>
2024-03-12 14:54:29 +0200

    Fix syntax error

--- pipeline/databases/1-first_table.sql
+++ pipeline/databases/1-first_table.sql
@@ -1,5 +1,4 @@
 -- create table if it doesn't exist
 CREATE TABLE IF NOT EXISTS first_table (
   id INT,
-  name VARCHAR(100),
-);
+  name VARCHAR(100));
--- pipeline/databases/4-best_score.sql
+++ pipeline/databases/4-best_score.sql
@@ -1,3 +1,5 @@
 -- lists records with score >= 10 and order by score(top first)
-SELECT * FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table
+WHERE score >= 10 
+ORDER BY score DESC;
 
