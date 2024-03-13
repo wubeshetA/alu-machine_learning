e808de26074ea691ee1f4f85588e7bc78a32fb00
Wubeshet Yimam <w.yimam@alustudent.com>
2024-03-13 14:22:42 +0200

    Add tas 12  13

--- pipeline/databases/9-no_genre.sql
+++ pipeline/databases/9-no_genre.sql
@@ -1,6 +1,6 @@
 -- join
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY title ASC, tv_show_genres.genre_id ASC
